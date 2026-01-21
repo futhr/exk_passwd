2de39c8e4ebaa4d8b4c07f37b7e5b5dec2d88e67
Tobias Bohwalli <hi@futhr.io>
2026-01-21 11:57:45 +0100

    fix: disable ex_unit to prevent duplicate test runs in mix check

--- .check.exs
+++ .check.exs
@@ -9,11 +9,12 @@
     {:compiler, "mix compile --warnings-as-errors"},
     {:formatter, "mix format --check-formatted"},
     {:credo, "mix credo --strict"},
+    {:ex_unit, false},
     {:test, command: "mix test", env: %{"MIX_ENV" => "test"}},
     {:hex_audit, "mix hex.audit"},
     {:mix_audit, "mix deps.audit"},
     {:dialyzer, false},
-    {:doctor, "mix doctor"},
+    {:doctor, "mix doctor --summary"},
     {:ex_doc, false},
     {:sobelow, false}
   ]
